7102720afc64043bcca2ebfe89b8d0496da763fd
T. J. O'Hearn <tohearn94@aol.com>
2015-03-05 13:30:44 -0500

    Update HourlyEmployee.java

--- Group1/Program5/src/HourlyEmployee.java
+++ Group1/Program5/src/HourlyEmployee.java
@@ -3,9 +3,7 @@ public class HourlyEmployee extends Employee{
     private double wage;
     HourlyEmployee(String name, int hours, double wage){
         super(name,hours);
-        this.hours = hours;
         this.wage = wage;
-        this.hours = hours;     
     }
     @Override
     public String getType(){
